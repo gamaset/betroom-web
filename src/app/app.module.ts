9980993b7b6fab9def7dee02c6b3a1545bd88d97
Christopher S. Rozario <christopherscotini@gmail.com>
2018-01-29 18:37:19 -0200

    iniciando template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './template/header/header.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { SideMenuComponent } from './template/side-menu/side-menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule
